d8f9a6211cd714eb0fe2468226140fd488c613e5
Pedro Rangel Caetano <p.r.caetano@gmail.com>
2020-05-13 14:08:13 -0300

    Fix error on radial vector during shift.

--- src/shift.cpp
+++ src/shift.cpp
@@ -70,13 +70,13 @@ void	shift_obj(std::vector<struct particle>& P,
     py+= (phi[Ng*Ng*ixp+Ng*iyP+izp]-phi[Ng*Ng*ixp+Ng*iy0+izp])*fac*wt;
     pz+= (phi[Ng*Ng*ixp+Ng*iyp+izP]-phi[Ng*Ng*ixp+Ng*iyp+iz0])*fac*wt;
 #ifdef DISTANT_OBSERVER_ZAXIS
-    float rx = box.ctr[0]+box.L*(P[nn].pos[0]-0.5);
-    float ry = box.ctr[1]+box.L*(P[nn].pos[1]-0.5);
-    float rz = box.ctr[2]+box.L*(P[nn].pos[2]-0.5);
-#else
     float rx = 0.;
     float ry = 0.;
     float rz = 1.;
+#else
+    float rx = box.ctr[0]+box.L*(P[nn].pos[0]-0.5);
+    float ry = box.ctr[1]+box.L*(P[nn].pos[1]-0.5);
+    float rz = box.ctr[2]+box.L*(P[nn].pos[2]-0.5);
 #endif
     float r2 = rx*rx+ry*ry+rz*rz;
     float pr = beta*(px*rx+py*ry+pz*rz);
